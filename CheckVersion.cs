fe1348ecfba27a803b280763d2b4b5ad93000ff8
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-08-07 23:46:41 +0200

    Update CheckVersion.cs

--- CheckVersion.cs
+++ CheckVersion.cs
@@ -1,24 +1,29 @@
 using System;
 using UnityEngine;
 
-public static void CheckVersion(Action<LoginResponse> response)
+public static partial class CloudAPI
 {
-    DateTime now = DateTime.Now;
-    DateTime midnight = now.Date.AddDays(1)
-    TimeSpan timeUntilMidnight = midnight - now;
+    private static readonly DateTime startDate = new DateTime(2025, 6, 14);
 
-    int daysSinceStart = (now.Date - startDate).Days;
-    int levelIndex = Mathf.Max(1, daysSinceStart + 1);
-
-    LoginResponse loginResponse = new LoginResponse
+    public static void CheckVersion(Action<LoginResponse> response)
     {
-        VersionOkay = true,
-        HoursUntilLevel = timeUntilMidnight.Hours,
-        MinutesUntilLevel = timeUntilMidnight.Minutes,
-        SecondsUntilLevel = timeUntilMidnight.Seconds,
-        LevelIndex = levelIndex,
-        Message = "why did the chicken cross the caldera?"
-    };
+        DateTime now = DateTime.Now;
+        DateTime midnight = now.Date.AddDays(1);
+        TimeSpan timeUntilMidnight = midnight - now;
 
-    response?.Invoke(loginResponse);
+        int daysSinceStart = (now.Date - startDate).Days;
+        int levelIndex = Mathf.Max(1, daysSinceStart + 1);
+
+        LoginResponse loginResponse = new LoginResponse
+        {
+            VersionOkay = true,
+            HoursUntilLevel = timeUntilMidnight.Hours,
+            MinutesUntilLevel = timeUntilMidnight.Minutes,
+            SecondsUntilLevel = timeUntilMidnight.Seconds,
+            LevelIndex = levelIndex,
+            Message = "why did the chicken cross the caldera?"
+        };
+
+        response?.Invoke(loginResponse);
+    }
 }
