c6f8576055da0cd72c05393da8b1e3062ce3dc99
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-08-07 13:19:49 +0200

    Update CheckVersion.cs

--- Beta Branch/CheckVersion.cs
+++ Beta Branch/CheckVersion.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 // Token: 0x02000074 RID: 116
 public static partial class CloudAPI
 {
-	private static int? cachedLevelIndex;
-	// Token: 0x0600042B RID: 1067 RVA: 0x00030930 File Offset: 0x0002EB30
-	public static void CheckVersion(Action<LoginResponse> response)
-	{
-		if (CloudAPI.cachedLevelIndex == null)
-		{
-			CloudAPI.cachedLevelIndex = new int?(global::UnityEngine.Random.Range(1, 1001));
-		}
-		LoginResponse loginResponse = new LoginResponse
-		{
-			VersionOkay = true,
-			HoursUntilLevel = 24,
-			MinutesUntilLevel = 00,
-			SecondsUntilLevel = 00,
-			LevelIndex = CloudAPI.cachedLevelIndex.Value,
-			Message = "Thanks for testing the PEAK beta. Watch out for bugs! (the current beta is the same as the live game, check back later for a new beta!)"
-		};
-		if (response != null)
-		{
-			response(loginResponse);
-		}
-	}
+    private static int? cachedLevelIndex;
+
+    // Token: 0x0600042B RID: 1067 RVA: 0x00030930 File Offset: 0x0002EB30
+    public static void CheckVersion(Action<LoginResponse> response)
+    {
+        if (CloudAPI.cachedLevelIndex == null)
+        {
+            System.Random rng = new System.Random();
+            CloudAPI.cachedLevelIndex = rng.Next(0, int.MaxValue); // random int >= 0
+        }
+
+        LoginResponse loginResponse = new LoginResponse
+        {
+            VersionOkay = true,
+            HoursUntilLevel = 24,
+            MinutesUntilLevel = 00,
+            SecondsUntilLevel = 00,
+            LevelIndex = CloudAPI.cachedLevelIndex.Value,
+            Message = "Thanks for testing the PEAK beta. Watch out for bugs! (the current beta is the same as the live game, check back later for a new beta!)"
+        };
+
+        response?.Invoke(loginResponse);
+    }
 }
