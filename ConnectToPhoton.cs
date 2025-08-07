7b0ccdd0c65bd928e1669e21f4a42c94bbad4d3c
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-08-07 13:18:43 +0200

    Removed useless code

--- ConnectToPhoton.cs
+++ ConnectToPhoton.cs
@@ -12,14 +12,5 @@ public partial class NetworkConnector : MonoBehaviourPunCallbacks
 	public static void ConnectToPhoton()
 	{
 		PhotonNetwork.OfflineMode = true;
-		BuildVersion version = new BuildVersion(Application.version);
-		PhotonNetwork.AutomaticallySyncScene = true;
-		PhotonNetwork.GameVersion = version.ToString();
-		PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = version.ToMatchmaking();
-		NetworkConnector.PrepareSteamAuthTicket(delegate
-		{
-			PhotonNetwork.ConnectUsingSettings();
-			Debug.Log("Photon Start" + PhotonNetwork.NetworkClientState.ToString() + " using app version: " + version.ToMatchmaking());
-		});
 	}
 }
