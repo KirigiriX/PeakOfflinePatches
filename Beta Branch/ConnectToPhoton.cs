e48f57840ca91a6d646a7fe12629ad98ff0d1f5b
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-08-07 21:45:21 +0200

    Update ConnectToPhoton.cs

--- Beta Branch/ConnectToPhoton.cs
+++ Beta Branch/ConnectToPhoton.cs
@@ -5,12 +5,21 @@ using Photon.Realtime;
 using UnityEngine;
 using Zorro.Core;
 
-// Token: 0x0200016B RID: 363
+// Token: 0x02000178 RID: 376
 public partial class NetworkConnector : MonoBehaviourPunCallbacks
 {
-	// Token: 0x060008F5 RID: 2293 RVA: 0x000421EC File Offset: 0x000403EC
+	// Token: 0x06000964 RID: 2404 RVA: 0x00044ADC File Offset: 0x00042CDC
 	public static void ConnectToPhoton()
 	{
 		PhotonNetwork.OfflineMode = true;
+		BuildVersion version = new BuildVersion(Application.version);
+		PhotonNetwork.AutomaticallySyncScene = true;
+		PhotonNetwork.GameVersion = version.ToString();
+		PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = version.ToMatchmaking();
+		NetworkConnector.PrepareSteamAuthTicket(delegate
+		{
+			PhotonNetwork.ConnectUsingSettings();
+			Debug.Log("Photon Start" + PhotonNetwork.NetworkClientState.ToString() + " using app version: " + version.ToMatchmaking());
+		});
 	}
 }
